016fc4fa9c12cef66481d0ef138fff519181bfe6
sunchi <sunchi@traceboard.com.cn>
2015-12-23 21:35:31 +0800

    add webPage

---
+++ webPage/js/dataSchool.js
@@ -0,0 +1,50 @@
+/**
+ * Created by Wilson on 2015/12/23.
+ */
+var dataSchool = {"data":[{"C_SID":"010f0c6554cc4ad98cc1377aa8991077","C_AREAID":"420582","C_AREANAME":"当阳市","C_CITYID":"420500","C_CITYNAME":"宜昌市","C_NAME":"当阳东群小学"},{"C_SID":"011fc56943f9432a8185319da9a816fa","C_AREAID":"420101","C_AREANAME":"市辖区","C_CITYID":"420100","C_CITYNAME":"武汉市","C_NAME":"234"},{"C_SID":"0ae063ea15dc45f3822d4be5b7c19b48","C_AREAID":"420102","C_AREANAME":"江岸区","C_CITYID":"420100","C_CITYNAME":"武汉市","C_NAME":"湖北大学附属中学"},{"C_SID":"1ba19ddf11854a01a602ac1ae511ff67","C_AREAID":"420101","C_AREANAME":"市辖区","C_CITYID":"420100","C_CITYNAME":"武汉市","C_NAME":"学酷小学"},{"C_SID":"1f29ee1f414141b6a1a58420862feb1b","C_AREAID":"420103","C_AREANAME":"江汉区","C_CITYID":"420100","C_CITYNAME":"武汉市","C_NAME":"武汉市一中"},{"C_SID":"23044da0dff0479783fe89103462a595","C_AREAID":"420527","C_AREANAME":"秭归县","C_CITYID":"420500","C_CITYNAME":"宜昌市","C_NAME":"秭归县实验中学"},{"C_SID":"25b4a33bdd1444548f3a0a2208523578","C_AREAID":"420582","C_AREANAME":"当阳市","C_CITYID":"420500","C_CITYNAME":"宜昌市","C_NAME":"当阳一中"},{"C_SID":"28f6a557d96043a8af133816a883d940","C_AREAID":"420106","C_AREANAME":"武昌区","C_CITYID":"420100","C_CITYNAME":"武汉市","C_NAME":"武汉小学"},{"C_SID":"2dad55a7e7d044338a6e268915f9d349","C_AREAID":"420582","C_AREANAME":"当阳市","C_CITYID":"420500","C_CITYNAME":"宜昌市","C_NAME":"当阳育林学校"},{"C_SID":"31a094785e5348cc85bb6eb7e459e14a","C_AREAID":"420582","C_AREANAME":"当阳市","C_CITYID":"420500","C_CITYNAME":"宜昌市","C_NAME":"当阳庙前中小学"},{"C_SID":"330408a65c594184b98f85b9e869e186","C_AREAID":"420102","C_AREANAME":"江岸区","C_CITYID":"420100","C_CITYNAME":"武汉市","C_NAME":"武汉市第二中学"},
+    //{"C_SID":"3a2e790d83d34dadaafb89b6c571507b","C_AREAID":"140101","C_AREANAME":"-","C_CITYID":"140100","C_CITYNAME":"-","C_NAME":"测试电话数据"},
+    {"C_SID":"3ce11ae3a8d64f94b8c18555b9248f01","C_AREAID":"420582","C_AREANAME":"当阳市","C_CITYID":"420500","C_CITYNAME":"宜昌市","C_NAME":"当阳东群小学"},
+    //{"C_SID":"40d58bb0ca5841009f7b0468e406d813","C_AREAID":"210202","C_AREANAME":"-","C_CITYID":"210200","C_CITYNAME":"-","C_NAME":"武汉实验二小"},
+    {"C_SID":"414fb2528290495993a9f9f8ec2434b8","C_AREAID":"420102","C_AREANAME":"江岸区","C_CITYID":"420100","C_CITYNAME":"武汉市","C_NAME":"武汉市滑坡路小学"},{"C_SID":"4597d921fce1468a98c079854e1e55b1","C_AREAID":"420582","C_AREANAME":"当阳市","C_CITYID":"420500","C_CITYNAME":"宜昌市","C_NAME":"当阳二高"},{"C_SID":"4723713bda314d89a944e7dc8e839796","C_AREAID":"420106","C_AREANAME":"武昌区","C_CITYID":"420100","C_CITYNAME":"武汉市","C_NAME":"三道街小学"},
+    //{"C_SID":"491623f83e6a4a0eb701c35c44e916bb","C_AREAID":"0","C_AREANAME":"-","C_CITYID":"0","C_CITYNAME":"-","C_NAME":"1"},{"C_SID":"4b3df38b51b84a3f987ab6d963b72fa0","C_AREAID":"150101","C_AREANAME":"-","C_CITYID":"150100","C_CITYNAME":"-","C_NAME":"测试"},
+    {"C_SID":"5010b020d25c4bc9b67b468c7b755e19","C_AREAID":"420101","C_AREANAME":"市辖区","C_CITYID":"420100","C_CITYNAME":"武汉市","C_NAME":"武昌实验中学"},{"C_SID":"581b7c2abacb4773905b9f07fa6f398a","C_AREAID":"420527","C_AREANAME":"秭归县","C_CITYID":"420500","C_CITYNAME":"宜昌市","C_NAME":"秭归实验小学"},{"C_SID":"5868119a2e8e4ff5899b9a8f3f6824ec","C_AREAID":"420101","C_AREANAME":"市辖区","C_CITYID":"420100","C_CITYNAME":"武汉市","C_NAME":"武汉中学"},{"C_SID":"66a181863e5d4db3a8c4b0f6ad121460","C_AREAID":"420101","C_AREANAME":"市辖区","C_CITYID":"420100","C_CITYNAME":"武汉市","C_NAME":"武汉华师一附中"},{"C_SID":"6781e7baf567477fb4207e3ad4048bf2","C_AREAID":"420501","C_AREANAME":"市辖区","C_CITYID":"420500","C_CITYNAME":"宜昌市","C_NAME":"宜昌市第四中学"},{"C_SID":"72dd2589f8634ff7904ca27723e0a52d","C_AREAID":"420102","C_AREANAME":"江岸区","C_CITYID":"420100","C_CITYNAME":"武汉市","C_NAME":"春雷小班"},{"C_SID":"7665ada54f864cf9b55f0bc3a24ef5d6","C_AREAID":"420102","C_AREANAME":"江岸区","C_CITYID":"420100","C_CITYNAME":"武汉市","C_NAME":"武昌水果湖二小"},{"C_SID":"77560cc3cac5432a85d4b312b4981f22","C_AREAID":"420502","C_AREANAME":"西陵区","C_CITYID":"420500","C_CITYNAME":"宜昌市","C_NAME":"宜昌市第二中学"},{"C_SID":"7a01449ae73c496f96dc241080104190","C_AREAID":"420103","C_AREANAME":"江汉区","C_CITYID":"420100","C_CITYNAME":"武汉市","C_NAME":"武汉外国语学校"},{"C_SID":"7f9cfe23050b40b0874cf9e6e4a09269","C_AREAID":"420106","C_AREANAME":"武昌区","C_CITYID":"420100","C_CITYNAME":"武汉市","C_NAME":"武昌区中北路小学"},{"C_SID":"8224fde2e6c241168407512f9d86051c","C_AREAID":"420101","C_AREANAME":"市辖区","C_CITYID":"420100","C_CITYNAME":"武汉市","C_NAME":"武昌区第一小学"},
+    //{"C_SID":"836889c673e0456ebde1c9383f0b30a3","C_AREAID":"150101","C_AREANAME":"-","C_CITYID":"150100","C_CITYNAME":"-","C_NAME":"斯蒂芬"},
+    {"C_SID":"84281ae9f54c4e588768fb53712c8790","C_AREAID":"420502","C_AREANAME":"西陵区","C_CITYID":"420500","C_CITYNAME":"宜昌市","C_NAME":"宜昌市第八中学"},{"C_SID":"887a37c3c79e4c528a5eb45601e86b37","C_AREAID":"420527","C_AREANAME":"秭归县","C_CITYID":"420500","C_CITYNAME":"宜昌市","C_NAME":"秭归县第一中学"},
+    //{"C_SID":"88e149fd08b34f7598277f337795650a","C_AREAID":"150102","C_AREANAME":"-","C_CITYID":"150100","C_CITYNAME":"-","C_NAME":"测试学校"},{"C_SID":"8902d2b87ec8489593e7cf52b821e6af","C_AREAID":"150101","C_AREANAME":"-","C_CITYID":"150100","C_CITYNAME":"-","C_NAME":"秭归郭家坝一中"},
+    {"C_SID":"8a034830f67044b5969ad147e8f103a3","C_AREAID":"420527","C_AREANAME":"秭归县","C_CITYID":"420500","C_CITYNAME":"宜昌市","C_NAME":"秭归希望小学"},
+    //{"C_SID":"8a2d6d59cfe14d8788e57e0903f19bfa","C_AREAID":"150102","C_AREANAME":"-","C_CITYID":"150100","C_CITYNAME":"-","C_NAME":"测试学校2"},{"C_SID":"8c7328fdc8f04cb5a568c0e7a4d96d13","C_AREAID":"150101","C_AREANAME":"-","C_CITYID":"150100","C_CITYNAME":"-","C_NAME":"学校测试"},{"C_SID":"944fa5c3891c4dfba350e34dfa174c95","C_AREAID":"150101","C_AREANAME":"-","C_CITYID":"150100","C_CITYNAME":"-","C_NAME":"武汉市十九中"},
+    {"C_SID":"a5aca5b614014cb3be5724425896bd6c","C_AREAID":"420102","C_AREANAME":"江岸区","C_CITYID":"420100","C_CITYNAME":"武汉市","C_NAME":"武汉市常青实验小学"},
+    //{"C_SID":"a6dceb3f74ab4911a02a9d0deb89b87b","C_AREAID":"150101","C_AREANAME":"-","C_CITYID":"150100","C_CITYNAME":"-","C_NAME":"星河小学"}, {"C_SID":"abf665aa92b24351b8a6c69027ef5502","C_AREAID":"150101","C_AREANAME":"-","C_CITYID":"150100","C_CITYNAME":"-","C_NAME":"武汉市第十六中学"},{"C_SID":"b5678b3dbdab4244a2a2aeedcd98858c","C_AREAID":"150101","C_AREANAME":"-","C_CITYID":"150100","C_CITYNAME":"-","C_NAME":"电话为空"},{"C_SID":"bc6f023f110249d59a51cd0487662851","C_AREAID":"150101","C_AREANAME":"-","C_CITYID":"150100","C_CITYNAME":"-","C_NAME":"中山路小学"},
+    {"C_SID":"be3704c32f614c6bb064d532151df445","C_AREAID":"420102","C_AREANAME":"江岸区","C_CITYID":"420100","C_CITYNAME":"武汉市","C_NAME":"武汉十中"},{"C_SID":"c3f9b20c173b432396538f5d02017bf0","C_AREAID":"420115","C_AREANAME":"江夏区","C_CITYID":"420100","C_CITYNAME":"武汉市","C_NAME":"武汉第八所综合学校"},{"C_SID":"c6134215395f449197567c6a02303f09","C_AREAID":"420501","C_AREANAME":"市辖区","C_CITYID":"420500","C_CITYNAME":"宜昌市","C_NAME":"宜昌市西陵区得胜街小学"},{"C_SID":"c796d3179e6a4a3fae1e74eb8f2f56e4","C_AREAID":"420101","C_AREANAME":"市辖区","C_CITYID":"420100","C_CITYNAME":"武汉市","C_NAME":"武汉第九所十二年一贯制学校"},{"C_SID":"cf44361a774c4d27b679a96e7d0611e6","C_AREAID":"420582","C_AREANAME":"当阳市","C_CITYID":"420500","C_CITYNAME":"宜昌市","C_NAME":"当阳东群小学"},
+    //{"C_SID":"d7ea60492c33416ea5462454bde2466d","C_AREAID":"110108","C_AREANAME":"-","C_CITYID":"110100","C_CITYNAME":"-","C_NAME":"海淀实验小学"},
+    {"C_SID":"d812192c1f904981ad15ea2f09521cb1","C_AREAID":"420101","C_AREANAME":"市辖区","C_CITYID":"420100","C_CITYNAME":"武汉市","C_NAME":"学酷学校"},{"C_SID":"d910dfca31164297a994c650b898964d","C_AREAID":"420502","C_AREANAME":"西陵区","C_CITYID":"420500","C_CITYNAME":"宜昌市","C_NAME":"宜昌外国语学校"},{"C_SID":"dbaaa3dc91c543a8a5ca58134ee4c9fe","C_AREAID":"420103","C_AREANAME":"江汉区","C_CITYID":"420100","C_CITYNAME":"武汉市","C_NAME":"武汉市二十八中"},{"C_SID":"e2dd4e463d3741e1823ddb24fa08e1d1","C_AREAID":"420103","C_AREANAME":"江汉区","C_CITYID":"420100","C_CITYNAME":"武汉市","C_NAME":"武汉市二十八中"},
+    //{"C_SID":"e761e8e0d25f4a60ade0581dc3f4b989","C_AREAID":"150101","C_AREANAME":"-","C_CITYID":"150100","C_CITYNAME":"-","C_NAME":"北京四中"},
+    {"C_SID":"e891912f01bb45a8b44f86bbb9f729f8","C_AREAID":"420101","C_AREANAME":"市辖区","C_CITYID":"420100","C_CITYNAME":"武汉市","C_NAME":"武汉无敌小学"},
+    //{"C_SID":"f130576c337347178aad94b0c65a969c","C_AREAID":"150101","C_AREANAME":"-","C_CITYID":"150100","C_CITYNAME":"-","C_NAME":"宜昌市西陵区东山小学"},
+    {"C_SID":"f7e73aea70fa48e4ae19b8e39694b054","C_AREAID":"420582","C_AREANAME":"当阳市","C_CITYID":"420500","C_CITYNAME":"宜昌市","C_NAME":"当阳东群小学"},{"C_SID":"ff459403ea4943cb9e641d1fa1f93c6a","C_AREAID":"420582","C_AREANAME":"当阳市","C_CITYID":"420500","C_CITYNAME":"宜昌市","C_NAME":"当阳东群小学"}]};
+
+var dataCity = [];
+var dataArea = [];
+
+var hashCity = {};
+for(var i=0; i<dataSchool.data.length; i++){
+    var jsonData = {};
+    if(!hashCity[dataSchool.data[i].C_CITYID]){
+        jsonData[dataSchool.data[i].C_CITYID] = dataSchool.data[i].C_CITYNAME;
+        dataCity.push(jsonData);
+        hashCity[dataSchool.data[i].C_CITYID] = true;
+    };
+};
+
+var hashArea = {};
+for(var i=0; i<dataSchool.data.length; i++){
+    var jsonData = {};
+    var jsonDataCity = {};
+
+    if(!hashArea[dataSchool.data[i].C_AREAID]){
+        jsonData[dataSchool.data[i].C_CITYID] = [dataSchool.data[i].C_AREAID,dataSchool.data[i].C_AREANAME];
+        dataArea.push(jsonData);
+        hashArea[dataSchool.data[i].C_AREAID] = true;
+    };
+};
+//alert(JSON.stringify(dataArea));
